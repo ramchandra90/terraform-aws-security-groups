8405b6df9bcfd02f5a680d4582c03946525509b4
ramachandra <ramchandra081@gmail.com>
2024-12-26 12:31:39 +1300

    functions

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+    default = {}
+}
+
+variable "environment" {
+    default = {}
+}
+
+variable "sg_name" {
+
+}
+
+variable "common_tags" {
+    default = {}
+}
+
+variable "sg_tags" {
+    default = {}
+}
+
+variable "vpc_id" {
+    default = {}
+}
